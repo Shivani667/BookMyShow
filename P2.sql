5b19b85d9078b61ed46681e3e52aa216f7b75107
Shivani Thakur <shivani7335@gmail.com>
2026-07-18 20:49:37 +0530

    Empty main branch

--- P2.sql
+++
@@ -1,6 +0,0 @@
-SELECT m.title, s.show_time
-FROM Shows s
-JOIN Movie m ON s.movie_id = m.movie_id
-JOIN Theatre t ON s.theatre_id = t.theatre_id
-WHERE t.name = 'PVR Saket'
-  AND s.show_date = '2026-07-20';
